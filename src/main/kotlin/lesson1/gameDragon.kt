2646bc83787e7c846bcc739d6189978c8dfba5b9
67W <error4434@gmail.com>
2022-04-11 01:36:22 +0300

    Задание: волшебное зеркало

--- src/main/kotlin/lesson1/gameDragon.kt
+++ src/main/kotlin/lesson1/gameDragon.kt
@@ -1,9 +1,14 @@
 package lesson1
 
 fun main() {
+    val playerName = "Estragon"
+
     var hasSteed: String
     val tavernName = "Рог единорога"
     val barmenName = "Трактирщик"
     var coins = 0
     var menu: List<String>
+    val mirror = playerName.reversed()
+    println(mirror)
+
 }
