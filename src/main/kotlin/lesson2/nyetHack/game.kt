b5185009367dbe273494925935dc2f722832a0b8
67W <error4434@gmail.com>
2022-04-11 01:57:22 +0300

    Операторы if/else

--- src/main/kotlin/lesson2/nyetHack/game.kt
+++ src/main/kotlin/lesson2/nyetHack/game.kt
@@ -2,7 +2,7 @@ package lesson2.nyetHack
 
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 100
+    var healthPoints = 89
     if (healthPoints == 100) {
         println(name + " is in excellent condition!")
     } else {
