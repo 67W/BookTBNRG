7ea596dcedc5f059254f3d9a8376af8dbfbffd4c
67W <error4434@gmail.com>
2022-04-11 01:28:52 +0300

    test2

--- src/main/kotlin/lesson1/typeIntro.kt
+++ src/main/kotlin/lesson1/typeIntro.kt
@@ -1,7 +1,10 @@
 package lesson1
 
+const val MAX_EXPERIENCE: Int = 5000
 fun main(args: Array<String>) {
+    var playerName = "Estragon"
     var experiencePoints: Int = 5
     experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
 }
