1ccab94f15637735f0aebce7b07d1dc5aab2cc17
67W <error4434@gmail.com>
2022-04-11 04:23:04 +0300

    Выделение кода в функции alt+ctrl+M

--- src/main/kotlin/lesson2/nyetHack/game.kt
+++ src/main/kotlin/lesson2/nyetHack/game.kt
@@ -9,6 +9,14 @@ fun main(args: Array<String>) {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
 
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    // Состояние игрока
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
@@ -20,8 +28,5 @@ fun main(args: Array<String>) {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    // Состояние игрока
-    println("(Aura: $auraColor) " +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+    return healthStatus
 }
